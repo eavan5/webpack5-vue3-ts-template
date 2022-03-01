19468e870b3a900205eaa95e50a08f3dba51b8e8
wumao <webwwy@gmail.com>
2022-03-01 16:05:47 +0800

    chore(webpack.config.ts): 清空dist插件

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const path = require('path')
 module.exports = {
 	mode: 'development',
@@ -10,6 +11,14 @@ module.exports = {
 	resolve: {
 		extensions: ['.js', 'ts', 'tsx'], // 指定文件扩展名
 	},
+	// webpack升级到5.0后，target默认值值会根据package.json中的browserslist改变，导致devServer的自动更新失效
+	// 所以 development 环境下直接配置成 web
+	target: 'web',
+	devServer: {
+		hot: true, // 启用热模块替换
+		open: true, // 打开默认浏览器
+	},
+	devtool: 'eval-cheap-module-source-map', // 精简sourcemap
 	module: {
 		rules: [
 			{
@@ -25,5 +34,6 @@ module.exports = {
 			title: 'webpack5-ts-vue',
 			template: './src/tpl/index.html',
 		}),
+		new CleanWebpackPlugin(), // 这个是用来清空之前打包的文件
 	],
 }
