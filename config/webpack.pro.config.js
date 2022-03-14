40eb1e0dfa988b91de87bad5d6062adc8a77ce0a
wumao <webwwy@gmail.com>
2022-03-15 00:33:40 +0800

    chore: 打包brotli压缩文件

--- config/webpack.pro.config.js
+++ config/webpack.pro.config.js
@@ -1,9 +1,16 @@
 const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+var BrotliPlugin = require('brotli-webpack-plugin');
 
 module.exports = {
   mode: 'production',
   plugins: [
     new CleanWebpackPlugin(), // 这个是用来清空之前打包的文件
+    new BrotliPlugin({
+      asset: '[path].br[query]',
+      test: /\.(js|css|html|svg)$/,
+      threshold: 10,
+      minRatio: 0.8
+    }),
   ]
 }
